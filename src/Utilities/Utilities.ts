288522e3df32d94883b9f3a461a294c7bd50288c
Giovanni Carvelli <giovannircarvelli@gmail.com>
2017-02-28 16:06:06 -0500

    Fix not being able to look at items inside containers

--- src/Utilities/Utilities.ts
+++ src/Utilities/Utilities.ts
@@ -2,13 +2,26 @@ import { Item } from "../Models/Models";
 
 export class Utilities {
     public static FindItemByName(items: Item[], name: string) {
-        let matches = items.filter(function(item) {
+        // look at items in list
+        let matches = items.filter(item => {
             return item.name == name;
         });
         if (matches.length > 0) {
             return matches[0];
-        } else {
-            return null;
         }
+
+        // Look at contents of items in list
+        items.forEach(item => {
+            if (item.contents && item.contents.length > 0) {
+                matches.push.apply(matches, item.contents.filter(con => {
+                    return con.name == name;
+                }));
+            }
+        });
+        if (matches.length > 0) {
+            return matches[0];
+        }
+
+        return null;
     }
 }
