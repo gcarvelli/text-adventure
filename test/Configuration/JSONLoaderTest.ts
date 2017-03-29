352a1a24e041065dc45086fe5350e5725da8b140
Giovanni Carvelli <giovannircarvelli@gmail.com>
2017-03-29 00:19:42 -0400

    Add some tests for JSONLoader

---
+++ test/Configuration/JSONLoaderTest.ts
@@ -0,0 +1,143 @@
+import "mocha";
+import { assert } from "chai";
+
+import { Generator } from "../Generator";
+import { Config } from "../../src/Configuration/Config";
+import { JSONLoader } from "../../src/Configuration/JSONLoader";
+
+
+describe("JSONLoader", () => {
+    let config: Config;
+    let data: any;
+
+    beforeEach(() => {
+        config = null;
+        data = { };
+        data.game = { };
+        data.help = [ ];
+        data.player = { };
+        data.rooms = { };
+        data.items = { };
+        data.npcs = { };
+        data.dialog_trees = { };
+        data.dialog_options = { };
+    });
+
+    let Load = function() {
+        let loader = new JSONLoader();
+        loader.Initialize(data);
+        config = loader.LoadConfig();
+    }
+
+    describe("Game", () => {
+        it("loads game name and version", () => {
+            data.game.name = "game name";
+            data.game.version = "game version";
+
+            Load();
+
+            assert.equal(config.game.name, data.game.name);
+            assert.equal(config.game.version, data.game.version);
+        });
+    });
+
+    describe("Help", () => {
+        it("loads help", () => {
+            data.help = [ "line 1", "line 2" ];
+
+            Load();
+
+            assert.equal(config.help.length, data.help.length);
+            for (let i = 0; i < config.help.length; i++) {
+                assert.equal(config.help[i], data.help[i]);
+            }
+        });
+
+        it("loads missing help", () => {
+            data.help = undefined;
+            Load();
+        });
+    });
+
+    describe("Player", () => {
+        it("loads missing inventory", () => {
+            Load();
+        });
+
+        it("loads empty inventory", () => {
+            data.player.items = [ ];
+            Load();
+        });
+
+        it("loads items in inventory", () => {
+            let itemData = Generator.NewItemData();
+            data.items[itemData.id] = itemData;
+            data.player.items = [ itemData.id ];
+
+            Load();
+
+            assert.equal(config.player.inventory.length, 1);
+            assert.equal(config.player.inventory[0].id, itemData.id);
+        });
+    });
+
+    describe("Items", () => {
+        it("loads basic item", () => {
+            let itemData = Generator.NewItemData();
+            data.items[itemData.id] = itemData;
+
+            Load();
+
+            assert.isNotNull(config.items[itemData.id]);
+            assert.equal(config.items[itemData.id].id, itemData.id);
+            assert.equal(config.items[itemData.id].name, itemData.name);
+            assert.equal(config.items[itemData.id].description, itemData.description);
+        });
+
+        it("loads item with take", () => {
+            let itemData = Generator.NewItemData();
+            itemData.take = {
+                can_take: true
+            };
+            data.items[itemData.id] = itemData;
+
+            Load();
+
+            assert.isNotNull(config.items[itemData.id]);
+            assert.isNotNull(config.items[itemData.id].take);
+            assert.isTrue(config.items[itemData.id].take.canTake);
+        });
+
+        it("loads item with open (and lock)", () => {
+            let contentData = Generator.NewItemData();
+            data.items[contentData.id] = contentData;
+
+            let itemData = Generator.NewItemData();
+            itemData.open = {
+                can_open: true,
+                contains_items: [ contentData.id ],
+                lock: {
+                    can_lock: true,
+                    key_id: Generator.RandomString()
+                }
+            };
+            data.items[itemData.id] = itemData;
+
+            Load();
+
+            let item = config.items[itemData.id];
+            assert.isNotNull(item);
+            assert.isNotNull(item.open);
+            assert.isTrue(item.open.canOpen);
+
+            assert.isNotNull(item.open.contents);
+            assert.isArray(item.open.contents);
+            assert.equal(item.open.contents.length, 1);
+            assert.equal(item.open.contents[0].id, contentData.id);
+
+            assert.isNotNull(item.open.lock);
+            assert.isTrue(item.open.lock.canLock);
+            assert.equal(item.open.lock.keyId, itemData.open.lock.key_id);
+        });
+    });
+});
--- test/Generator.ts
+++ test/Generator.ts
@@ -1,11 +1,25 @@
 import { Item } from "../src/Models/Item";
 
 export class Generator {
-    public static NewItem() {
+    public static RandomString(): string {
+        return Math.random().toString(36).substring(7);
+    }
+    public static NewItem(): Item {
         let item = new Item();
-        item.id = "itemid";
-        item.name = "item_name";
+        item.id = this.RandomString();
+        item.name = this.RandomString();
+        item.description = this.RandomString();
         item.keywords = [ item.name ];
         return item;
+    }
+
+    public static NewItemData(): any {
+        let item = this.NewItem();
+        return {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            keywords: item.keywords
+        };
     }
 }
